2911b454e91e96db208a92fec4c0036011081e07
Betty Sosterics <betty.sosterics@gmail.com>
2023-09-04 14:33:37 +0200

    Change about information

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,15 +15,12 @@ function Home() {
         <div className="container">
           <div className="text-wrapper">
             <h1 data-test-id="pageTitle" className="title">
-              Halo, saya Lutfhi Ramadhan
+              Hi, my name is Betty
             </h1>
-            <p className="description">
-              Saya seorang pekerja lepas pengembang web, fotograper, desainer
-              antarmuka pengguna, dan penulis.
-            </p>
+            <p className="description">I am a software developer in progress</p>
 
             <Link href="/contact" className="cta">
-              Kontak Saya
+              contact me here
             </Link>
           </div>
 
